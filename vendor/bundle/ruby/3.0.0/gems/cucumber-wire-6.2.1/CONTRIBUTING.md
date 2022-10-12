2da0abb59df123642db03ce91604c76050566166
Reinaldo Neto <reinaldo.99souza@gmail.com>
2022-10-12 20:16:40 -0300

    Modified config/environments/test.rb

--- vendor/bundle/ruby/3.0.0/gems/cucumber-wire-6.2.1/CONTRIBUTING.md
+++
@@ -1,2 +0,0 @@
-Please see https://github.com/cucumber/cucumber-ruby/blob/main/CONTRIBUTING.md for
-more info about how to contribute to cucumber
--- vendor/bundle/ruby/3.0.0/gems/cucumber-wire-6.2.1/lib/cucumber/wire.rb
+++
@@ -1,7 +0,0 @@
-require 'cucumber/wire/plugin'
-
-return unless respond_to?(:InstallPlugin)
-
-InstallPlugin do |config, registry|
-  Cucumber::Wire::Plugin.new(config, registry).install
-end
