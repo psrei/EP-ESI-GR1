c8bd0e7d96b80253ade3ce2eb47383cce1fe82d6
VinZacarias <71726083+VinZacarias@users.noreply.github.com>
2022-11-09 20:52:22 -0300

    Inicio implementação página do aluno
    Início implementação dos testes e views

---
+++ funcoes_perfil_aluno1.rb
@@ -0,0 +1,12 @@
+ Dado('que estou na página de aluno') do
+    pending # TODO checar rotas
+  end
+  
+  Quando('e clico na opção de {escrever redação}') do
+    pending # TODO botão de redação
+  end
+  
+  Então('devo ser direcionado para página para {redigir a redação}') do
+    pending # TODO rota para página de redação
+  end
+  
