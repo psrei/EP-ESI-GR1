932b662237a05ad482488a50e38f66f0aa4a662f
VinZacarias <71726083+VinZacarias@users.noreply.github.com>
2022-11-23 19:49:48 -0300

    steps para perfil aluno e professor
    steps para perfil aluno e professor

---
+++ features/step_definitions/funcoes_perfil_aluno2.rb
@@ -0,0 +1,12 @@
+Dado('que estou na página de aluno') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('e clico na opção de escrever redação') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('devo ser direcionado para página para redigir a redação') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
---
+++ features/step_definitions/funcoes_perfil_professor1.rb
@@ -0,0 +1,11 @@
+Dado('que estou na página do professor') do
+    visit "/professor"
+  end
+  
+  Quando('e clico na opção de escrever redação') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('devo ser direcionado para página para redigir a redação') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
