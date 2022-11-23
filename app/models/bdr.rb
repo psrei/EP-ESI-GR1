63921746921c35fc6e51fc91cf68dea325800cef
Stefano Bisognin de Almeida <71796639+StefanoBAlmeida@users.noreply.github.com>
2022-11-23 16:16:43 -0300

    Add files via upload

---
+++ app/models/bdr.rb
@@ -0,0 +1,4 @@
+class Aluno < ApplicationRecord
+  validates :bdr, presence: { message: "É obrigatório ter uma banco de redacoes!" }
+  validates :redacao, presence: { message: "É obrigatório ter a redacao!" } 
+end
