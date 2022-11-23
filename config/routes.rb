7eee0b931c75ef33e097840f565c1712df9a4f0c
v1caum <114362467+v1caum@users.noreply.github.com>
2022-11-22 23:40:11 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,3 +4,13 @@ Rails.application.routes.draw do
     resources :redacoes
     root 'redacoes#new'
 end
+
+Rails.application.routes.draw do
+   root 'sessions#home'
+  resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
+  
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
+end
