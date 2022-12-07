42c69aa0c627248387a81543c303b2ef00c0f0be
Vinicius Yosimoto <114362467+v1caum@users.noreply.github.com>
2022-12-06 23:54:21 -0300

    Add files via upload

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+   root 'sessions#home'
+  resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
+  
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
+end
