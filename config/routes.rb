f46ed256c76f1ba81b3fd3f0d311f71a9394a8e8
Vinicius Yosimoto <114362467+v1caum@users.noreply.github.com>
2022-12-07 17:35:44 -0300

    Add files via upload

---
+++ config/routes.rb
@@ -0,0 +1,26 @@
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+Rails.application.routes.draw do
+    resources :redacao, only: [:new, :create, :edit, :update, :show, :destroy]
+    root 'redacao#new'
+end
+
+Rails.application.routes.draw do
+    resources :bdr, only: [:edit, :update, :show, :destroy]
+    #root 'bdr#view'
+end
+
+Rails.application.routes.draw do
+    #root 'sessions#home', as: nil
+    resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
+
+    get '/login', to: 'sessions#login'
+    post '/login', to: 'sessions#create'
+    post '/logout', to: 'sessions#destroy'
+    get '/logout', to: 'sessions#destroy'
+end
+
+Rails.application.routes.draw do
+    resources :editar, only: [:new, :create, :edit, :update, :show, :destroy]
+    root 'editar#new'
+end
