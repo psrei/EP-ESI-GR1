ab0d74260a64d6d9ecdc5566aef7ef33c1d56253
Vinicius Yosimoto <114362467+v1caum@users.noreply.github.com>
2022-12-07 16:59:42 -0300

    Delete routes.rb

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-   root 'sessions#home'
-  resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
-  
-  get '/login', to: 'sessions#login'
-  post '/login', to: 'sessions#create'
-  post '/logout', to: 'sessions#destroy'
-  get '/logout', to: 'sessions#destroy'
-end
