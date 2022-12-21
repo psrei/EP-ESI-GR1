602166eac09b4996cd7e3f2f6e7e1000beb860b5
Vinicius Yosimoto <114362467+v1caum@users.noreply.github.com>
2022-12-21 16:28:01 -0300

    Delete bdr.rb

--- app/models/bdr.rb
+++
@@ -1,4 +0,0 @@
-class Bdr < ApplicationRecord
-    validates :bdr, presence: { message: "É obrigatório ter uma banco de redacoes!" }
-    validates :redac, presence: { message: "É obrigatório ter a redacao!" }
-end
