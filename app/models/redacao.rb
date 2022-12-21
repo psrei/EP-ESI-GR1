cbf15689b7eea5ac9f93f51c2704ecf0e0891e9a
Vinicius Yosimoto <114362467+v1caum@users.noreply.github.com>
2022-12-21 16:28:10 -0300

    Delete redacao.rb

--- app/models/redacao.rb
+++
@@ -1,5 +0,0 @@
-class Redacao < ApplicationRecord
-    validates :redacao, presence: { message: "É obrigatório inserir o texto!" }
-    validates :texto, presence: { message: "É obrigatório inserir texto em português" }#, format: { with: URI::Generic::TEXT_REGEXP,
-      #message: "Caracteres de texto inválido!" }
-end
